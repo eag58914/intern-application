b82c21ad0591ffbda6cdd31b540b0138f6a8518b
Elco Garcia <garciaelco18@gmail.com>
2020-05-07 09:33:10 -0500

    Added functionality to change the colors randomly

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './Child.css';
 
-function Child() {
-	return <div>text</div>;
+function Child(props) {
+	function getRandomColor() {
+		var letters = '0123456789ABCDEF';
+		var color = '#';
+		for (var i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
+
+	return (
+		<div className="container">
+			<p style={{ color: props.color }}>text</p>
+			<button onClick={() => props.setColor(getRandomColor)}>Randomly change my color!</button>
+		</div>
+	);
 }
 
 export default Child;
